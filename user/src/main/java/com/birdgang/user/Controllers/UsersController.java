ed847ade4a343b870b89e0704bb5a302852e86f2
Will Stopczynski <wstopczynski@wisc.edu>
2023-04-17 15:46:26 -0500

    Revamped User, Added to app.prop, added to UserControl

--- user/src/main/java/com/birdgang/user/Controllers/UsersController.java
+++ user/src/main/java/com/birdgang/user/Controllers/UsersController.java
@@ -10,14 +10,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.birdgang.user.Models.User;
+import com.birdgang.user.Models.UserRepository;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/users/")
 public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
-    public List<User> list() {
+    public @ResponseBody List<User> list() {
         List<User> users = new ArrayList<>();
         return users;
     }
@@ -25,12 +32,25 @@ public class UsersController {
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody User user){
-
     }
 
     @GetMapping("/{id}")
     public User get(@PathVariable("id") long id){
         return new User();
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping(path="/add/{name}/{email}")
+    public @ResponseBody String addNewUser(@PathVariable("name") String name, @PathVariable("email") String email){
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
     }
 }
 
--- user/src/main/java/com/birdgang/user/Models/User.java
+++ user/src/main/java/com/birdgang/user/Models/User.java
@@ -1,171 +1,41 @@
 package com.birdgang.user.Models;
-/**
- * This class maps to the User table in the Five Course Feeder database.
- * It is used to carry out the basic CRUD operations on a single instance
- * from the table.
- * @author:  Carson Miller
- * @version: 2023.03.28
- */
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
 public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer pk;
 
-    // Instance variables
-    private int userID;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    
+  private String name;
 
-    /**
-     * This is an empty constructor for the User class.
-     * It is used to create an instance of the class without initializing
-     * any of the instance variables. This constructor is called when creating
-     * a new instance of the class to be inserted into the User table.
-     * @return an instance of the User class
-     */
-    public User(){
-        // validate existense of dbcxnmaker
+  private String email;
 
-        // creating an empty User instance
+  public Integer getPk() {
+    return pk;
+  }
 
-        // return User
-    }
+  public void setPk(Integer pk) {
+    this.pk = pk;
+  }
 
-    /**
-     * This is a constructor for the User class.
-     * It is used to create an instance of the class and initialize
-     * the instance variables with the values that correspond to the
-     * instance in the User table with the given primary key value.
-     * @param userID - the primary key value of the record in the User table
-     * @return an instance of the User class
-     */
-    public User(int userID){
-        // validate existense of dbcxnmaker
+  public String getName() {
+    return name;
+  }
 
-        // use primary key to access existing row in table 
-        
-        // use to set values of instance vars
+  public void setName(String name) {
+    this.name = name;
+  }
 
-        // return User
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    // Getters and Setters
-    public String getUsername(){
-        return this.username;
-    }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return this.password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public String getFirstName(){
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
-
-    public String getLastName(){
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName){
-        this.lastName = lastName;
-    }
-
-    
-    /**
-     * This method executes an INSERT INTO statement on the This table.
-     * It creates a new record in the table.
-     * It could be called by the save() method.
-     */
-    private void create(){
-        // use JDBC Connection to initialize Statement object
-        
-        // execute INSERT INTO query on Statement object using instance variables
-    }
-    
-    /**
-     * This method executes a SELECT statement on the This table.
-     * It selects all columns for a particular primary key value.
-     * It is called by the load() method.
-     * @param:  userID - the primary key value of the record to be read
-     */
-    private void read(int userID){
-        // use JDBC Connection to initialize Statement object
-        
-        // execute SELECT query on Statement object using primary key variable
-        
-        // set instance variables to results from query
-    }
-    
-    /**
-     * This method executes an UPDATE statement on the User table.
-     * It updates the table with the values of the instance variables
-     * in this User object.
-     * It could be called by the save() method.
-     */
-    private void update(){
-        // use JDBC Connection to initialize Statement object
-        
-        // execute UPDATE query on Statement object using instance variables
-    }
-    
-    /**
-     * This method executes a DELETE statement on the User table.
-     * It deletes the record for this instance of the User class.
-     * It is called by the remove() method.
-     */
-    private void delete(){
-        // use JDBC Connection to initialize Statement object
-        
-        // execute DELETE query on Statement object using instance's primary key
-    }
-
-    /**
-     * This method calls create() if the userID does not exist in the User table or
-     * update() if the userID exists in the User table
-     */
-    public void save(){
-        // validate existence of primary key in table
-
-        /*
-         * MySQL Statement to execute:
-         * SELECT EXISTS(SELECT * from Users WHERE User_id=userID);
-         * 
-         * Store result in boolean variable called exists
-         */
-
-        // if exists:
-            update();
-        // else:
-            create();
-    }
-
-    /**
-     * This method calls read() to "pick up" the record for a particular primary key value.
-     * @param:  userID - the primary key value of the record to be read
-     */
-    public void load(int userID){
-        // call read() to pick up the record corresponding to the userID
-        read(this.userID);
-    }
-
-    /**
-     * This method calls delete() to remove this instance of the User
-     * class from the User table.
-     */
-    public void remove(){
-        // call delete() to remove record for this instance of the table
-        delete();
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
