f6c23028445138e2d0a46fe4f3a008cc0272ec3e
Carson Miller <carsonrams34@gmail.com>
2023-04-19 18:29:46 -0500

    want to pass parameters in request body

--- user/src/main/java/com/birdgang/user/user/Controllers/UsersController.java
+++ user/src/main/java/com/birdgang/user/user/Controllers/UsersController.java
@@ -23,15 +23,55 @@ public class UsersController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping
-    public @ResponseBody List<User> list() {
-        List<User> users = new ArrayList<>();
-        return users;
+    ///////////////////////////////////////////////////////////////////////////////
+    //                                                                           //
+    //  WOULD RATHER PASS PARAMETERS IN THE BODY OF THE REQUEST THAN IN THE URL  //
+    //                                                                           //
+    ///////////////////////////////////////////////////////////////////////////////
+    /**
+     * This method is used to register a user
+     * @param username
+     * @param password
+     * @param firstName
+     * @param lastName
+     * @return HttpStatus.OK if the user was successfully registered,
+     *          HttpStatus.CONFLICT if the username already exists
+     */
+    @PostMapping(path="/register")
+    public @ResponseStatus String register(@RequestBody String username, String password, String firstName, String lastName){
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        if(userRepository.findByUsername(username) != null)
+            return HttpStatus.CONFLICT.toString();
+        else {
+            userRepository.save(user);
+            return HttpStatus.OK.toString();
+        }   
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void create(@RequestBody User user){
+    /**
+     * This method is used to login a user
+     * @param username
+     * @param password
+     * @return HttpStatus.OK if the user exists and the password is correct,
+     *          HttpStatus.NOT_FOUND if the user does not exist,
+     *          HttpStatus.UNAUTHORIZED if the password is incorrect
+     */
+    @PostMapping(path="/login")
+    public @ResponseStatus String login(@RequestBody String username, String password){
+        // HOW DO I CHECK IF THE USER EXISTS IN THE DATABASE?
+        // HOW DO I CHECK IF THE PASSWORD IS CORRECT?
+        // IDEALLY, THERE WOULD BE A METHOD IN THE REPOSITORY THAT WOULD DO THIS FOR ME
+        // BUT I DON'T KNOW HOW TO DO THAT
+        if(userRepository.findByUsername(username) == null)
+            return HttpStatus.NOT_FOUND.toString();
+        else if(userRepository.findByUsername(username).getPassword() != password)
+            return HttpStatus.UNAUTHORIZED.toString();
+        else
+            return HttpStatus.OK.toString();
     }
 
     @GetMapping("/{id}")
@@ -43,11 +83,12 @@ public class UsersController {
         return this.userRepository;
     }
 
-    @GetMapping(path="/all")
+    @GetMapping
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    // DON'T LIKE PASSING THE PARAMETERS IN THE URL
     @PostMapping(path="/add/{username}/{password}/{firstName}/{lastName}")
     public @ResponseBody String addNewUser(@PathVariable("username") String username, @PathVariable("password") String password,
                                             @PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName){
